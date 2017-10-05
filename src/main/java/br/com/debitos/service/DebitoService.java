5706ef5959d5b54729e74b503efd68f07309cfac
Fabricio Vallim <fmvallim@gmail.com>
2017-10-04 21:52:09 -0300

    Criando serviço de debito com métodos para salvar e listar debitos

---
+++ src/main/java/br/com/debitos/service/DebitoService.java
@@ -0,0 +1,23 @@
+package br.com.debitos.service;
+
+import br.com.debitos.domain.orm.Debito;
+import br.com.debitos.domain.repository.DebitoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DebitoService {
+
+    @Autowired
+    private DebitoRepository debitoRepository;
+
+    public List<Debito> findAll() {
+        return debitoRepository.findAll();
+    }
+
+    public Debito save(Debito debito) {
+        return debitoRepository.save(debito);
+    }
+}
