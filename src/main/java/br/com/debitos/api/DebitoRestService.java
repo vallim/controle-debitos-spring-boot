d78c44c4e82e4f175209270901e915ca59808473
Fabricio Vallim <fmvallim@gmail.com>
2017-10-04 21:52:54 -0300

    Alterando api rest para retornar dados do repositório ao invés de dados estáticos

--- src/main/java/br/com/debitos/api/DebitoRestService.java
+++ src/main/java/br/com/debitos/api/DebitoRestService.java
@@ -1,33 +1,31 @@
 package br.com.debitos.api;
 
 import br.com.debitos.domain.orm.Debito;
+import br.com.debitos.service.DebitoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
 
-@RequestMapping("/debitos")
 @RestController
+@RequestMapping("/debitos")
 public class DebitoRestService {
 
-    private static List<Debito> debitos = new ArrayList<>();
-
-    static {
-        Debito debitoUm = Debito.builder().id(1L).nome("conta de agua")
-                .valor(new BigDecimal("67.80"))
-                .vencimento(LocalDate.now()).build();
-
-        debitos.add(debitoUm);
-    }
+    @Autowired
+    private DebitoService debitoService;
 
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<List<Debito>> list() {
-        return ResponseEntity.ok(debitos);
+        return ResponseEntity.ok(debitoService.findAll());
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Debito> save(@RequestBody Debito debito) {
+        return ResponseEntity.ok(debitoService.save(debito));
     }
 }
